2a0463fe215f637f599b6209cf7f20f7a2bc1e2d
Dia6lo <Dgamjm@gmail.com>
2016-12-18 22:45:07 +1000

    Add test gh-pages.

---
+++ docs/scripts/bridge.pixi.external.js
@@ -0,0 +1,16 @@
+﻿(function (globals) {
+    "use strict";
+
+    /** @namespace Bridge.Pixi.External */
+    
+    /**
+     * https://github.com/primus/eventemitter3
+     *
+     * @abstract
+     * @public
+     * @class Bridge.Pixi.External.EventEmitter
+     */
+    Bridge.define('Bridge.Pixi.External.EventEmitter');
+    
+    Bridge.init();
+})(this);
